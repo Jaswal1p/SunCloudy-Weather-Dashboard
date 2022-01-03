ab570966025f5d24f7514505563dfcd126e7f22d
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-03 06:22:38 -0500

    coded for a cityWeather which works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,39 @@
-let key = "e52b9ef91ee8442a5c05329478d22900";
-let geoLocation = "lat=33.44&lon=-94.04"
+let weather = {
+    apiKey: "e52b9ef91ee8442a5c05329478d22900",
+    fetch: function() {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/onecall?" + lat + "&" + lon + "&exclude=hourly,daily&appid=" + this.apiKey
+        )    
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    
+    displayWeather: function(data) {
+
+    }
+}
+
+let cityWeather = {
+    apiKey: "61d80f3cab144660935d5755dd2fb631",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    
+    displayWeather: function(data) {
+
+    }
+}
+
 let userFormEl = document.querySelector("#user-form");
 let nameInputEl = document.querySelector("#city");
+let temperature = document.querySelector("#temperature");
+
+
+
 
 let formSubmitHandler = function(event) {
     event.preventDefault();
@@ -15,21 +47,8 @@ let formSubmitHandler = function(event) {
       
 };
 
-let getUserCities = function() {
-    
-    let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?" + geoLocation + "&exclude=minutely,hourly&appid=e52b9ef91ee8442a5c05329478d22900";
 
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-            });
-        }
-    });
-    
-    console.log("response");
-  };
   
-}
+}  
 
 userFormEl.addEventListener("submit", formSubmitHandler);
